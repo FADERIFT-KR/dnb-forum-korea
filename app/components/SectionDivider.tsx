3afb0be3bfe0670a0957a7cfafe74b24f129cd32
FADERIFT-KR <jazzlike455@gmail.com>
2026-01-19 16:57:11 +0900

    Create SectionDivider.tsx

---
+++ app/components/SectionDivider.tsx
@@ -0,0 +1,13 @@
+export default function SectionDivider() {
+  return (
+    <div
+      style={{
+        width: "92%",
+        maxWidth: 1400,
+        height: 1,
+        backgroundColor: "#d4d4d4",
+        margin: "32px auto 48px auto",
+      }}
+    />
+  )
+}
